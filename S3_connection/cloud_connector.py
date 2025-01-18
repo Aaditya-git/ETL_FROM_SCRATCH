977ce1295cdc2fa3638beaaf401637f4096418a3
Aaditya Bhilegaonkar <aadityab134@gmail.com>
2025-01-17 23:17:49 -0700

    Todays push

--- S3_connection/cloud_connector.py
+++ S3_connection/cloud_connector.py
@@ -6,6 +6,7 @@ class S3_connector:
 
     s3_client = boto3.client('s3')
     def create_buck_if_not_exists(self,name):
+        print("Inside Create function")
         try:
             print(f"Received bucket name: {name}")
             res=self.s3_client.head_bucket(Bucket=name)
@@ -22,8 +23,10 @@ class S3_connector:
             )
 
     # takes the name of the file to be uploaded, bucket name, and key i.e. folder name as input 
-    def upload(self,file_name,buk_name,folder_name):
+    # def upload(self,file_name,buk_name,folder_name):
+    def upload(self,file_name,buk_name):
         #construct file path
+        print("Inside Upload function")
         hard_path = r'C:\Clutchgod\S3_connector\Input Files'
         file_path = os.path.join(hard_path,file_name)
         print(file_path)
@@ -33,17 +36,17 @@ class S3_connector:
             raise FileNotFoundError(f"File {file_name} does not exist in {hard_path}")
 
         #Generating file path 
-        folder_to_be_uploaded = folder_name
+        # folder_to_be_uploaded = folder_name
         key = file_name
-        final_name_path = f"{folder_to_be_uploaded}/{key}"
-        print(final_name_path)
+        # final_name_path = f"{folder_to_be_uploaded}/{key}"
+        # print(final_name_path)
 
         #Check if the bucket exists or not! 
         try:
             bucket_name = buk_name
             self.s3_client.head_bucket(Bucket=buk_name)
             print(f"Bucket {buk_name} exists")
-            self.s3_client.upload_file(file_path,bucket_name,final_name_path)
+            self.s3_client.upload_file(file_path,bucket_name,key)
         except self.s3_client.exceptions.ClientError as e:
             error_code = e.response['Error']['Code']
             if error_code == '404':
@@ -53,8 +56,9 @@ class S3_connector:
             else:
                 print(f"Error accessing bucket '{bucket_name}': {e}")
      
-     
+
     def download(self):
+        print("Inside download function")
         hard_path = r'C:\Clutchgod\S3_connector\downloaded_files'
         key = 'raw_file.csv'
         hard_path_final = f"{hard_path}/{key}"
@@ -63,18 +67,24 @@ class S3_connector:
         s3_client.download_file('medallion-bucket', 'bronze/BigMart Sales.csv', hard_path_final)
 
 
-    def copy_from_one_bucket_to_another(self,source_bucket,output_bucket)
+    def copy_from_one_bucket_to_another(self,source_bucket,output_bucket):
+        print("Inside copy function")
         s3 = boto3.resource('s3')
         copy_source = {
             'Bucket': source_bucket,
-            'Key': 'mykey'
+            'Key': 'BigMart Sales.csv'
         }
-        s3.meta.client.copy(copy_source, output_bucket, 'otherkey')
+        s3.meta.client.copy(copy_source, output_bucket, 'bronze/rawfile')
+
 
 input_bucket_name = 'input-source-bucket-for-etl'
 fetching_bucket = 'medallion-bucket'
+
 obj = S3_connector()
-# obj.create_buck_if_not_exists(fetching_bucket)
-# obj.upload('BigMart Sales.csv',fetching_bucket,'bronze')
-obj.download()
+obj.create_buck_if_not_exists(fetching_bucket)
+obj.create_buck_if_not_exists(input_bucket_name)
+obj.upload('BigMart Sales.csv',input_bucket_name)
+obj.copy_from_one_bucket_to_another(input_bucket_name,fetching_bucket)
+
+# obj.download()
 
